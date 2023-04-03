068c7bfd457dd6c6c5f7bd2510ece78ad9a0a3bb
naxy24 <nurullah.gnd65@gmail.com>
2023-04-03 11:16:57 +0300

    Decorator Pattern

---
+++ Decorator Pattern/Decorator Pattern/Employee.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Pattern
+{
+    public class Employee
+    {
+        public void UserAdding()
+        {
+            Console.WriteLine($"{GetType().Name} Welcome to company");
+
+        }
+        public void UserRemoved()
+        {
+            Console.WriteLine($"{GetType().Name} Goodbye. We miss you");
+        }
+    }
+    public class EmployeeDecorator : Employee
+    {
+        public virtual void UserAdding()
+        {
+            //do something
+            base.UserAdding();
+        }
+        public virtual void UserRemoved()
+        {
+            base.UserRemoved();
+            //do something
+        }
+    }
+    public class Roles : EmployeeDecorator
+    {
+
+        public override void UserAdding()
+        {
+            base.UserAdding();
+            Console.WriteLine("You are new Admin!");
+        }
+        public override void UserRemoved()
+        {
+            Console.WriteLine("Your permission denied.");
+            base.UserRemoved();
+            
+        }
+    }
+    public class Intern :EmployeeDecorator
+    {
+        public override void UserAdding()
+        {
+            base.UserAdding();
+            Console.WriteLine($"{GetType().Name} Your internship is started");
+        }
+        public override void UserRemoved()
+        {
+            Console.WriteLine($"{GetType().Name} Your internship is Ended");
+            base.UserRemoved();
+            
+        }
+    }
+}
---
+++ Decorator Pattern/Decorator Pattern/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Decorator_Pattern;
+
+Console.WriteLine("Hello, World!");
+Employee employee = new Employee();
+employee.UserAdding();
+Console.WriteLine("-----");
+employee.UserRemoved();
+Console.WriteLine("==============================================");
+Roles roles = new Roles();
+roles.UserAdding();
+Console.WriteLine("-----");
+roles.UserRemoved();
+Console.WriteLine("==============================================");
+Intern intern = new Intern();
+intern.UserAdding();
+Console.WriteLine("-----");
+intern.UserRemoved();
+
+//Console.ReadKey();
